ed2651324cfe938f9cc1c6015f1425af24ea2a5b
James Woolfenden <james.woolfenden@gmail.com>
2022-06-29 20:51:37 +0100

    with api wrapper

--- main.go
+++ main.go
@@ -1,17 +1,73 @@
 package main
 
 import (
+	"fmt"
 	"github.com/jameswoolfenden/pike/src"
+	"github.com/urfave/cli/v2"
 	"io/ioutil"
 	"log"
+	"os"
+	"sort"
 )
 
 func main() {
-	dirname := "./terraform/"
+	var language string
+	var directory string
+
+	app := &cli.App{
+		EnableBashCompletion: true,
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "config",
+				Aliases: []string{"c"},
+				Usage:   "Load configuration from `FILE`",
+			},
+			&cli.StringFlag{
+				Name:        "directory",
+				Aliases:     []string{"D"},
+				Usage:       "Directory to scan",
+				Destination: &directory,
+			},
+		},
+		Commands: []*cli.Command{
+			{
+				Name:    "scan",
+				Aliases: []string{"s"},
+				Usage:   "scan",
+				Action: func(*cli.Context) error {
+					return scan(directory)
+				},
+			},
+		},
+		Name:  "pike",
+		Usage: "Generate IAM policy from your IAC code",
+		Action: func(cCtx *cli.Context) error {
+			name := "someone"
+			if cCtx.NArg() > 0 {
+				name = cCtx.Args().Get(0)
+			}
+			if language == "spanish" {
+				fmt.Println("Hola", name)
+			} else {
+				fmt.Println("Hello", name)
+			}
+			return nil
+		},
+	}
+
+	sort.Sort(cli.FlagsByName(app.Flags))
+	sort.Sort(cli.CommandsByName(app.Commands))
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func scan(dirname string) error {
 	files, err := ioutil.ReadDir(dirname)
 
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	var results []template
@@ -27,6 +83,7 @@ func main() {
 	}
 
 	log.Print(results)
+	return nil
 }
 
 type template struct {
