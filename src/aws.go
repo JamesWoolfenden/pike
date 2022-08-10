63a4214d78ee44f167c10f3fcd309572d78b832f
James Woolfenden <james.woolfenden@gmail.com>
2022-08-10 22:09:01 +0100

    case to lookup table

--- src/aws.go
+++ src/aws.go
@@ -20,101 +20,62 @@ func GetAWSPermissions(result ResourceV2) []string {
 
 // GetAWSResourcePermissions looks up permissions required for resources
 func GetAWSResourcePermissions(result ResourceV2) []string {
-	var Permissions []string
-	switch result.Name {
-	case "aws_s3_bucket":
-		Permissions = GetPermissionMap(awsS3Bucket, result.Attributes)
-	case "aws_s3_bucket_acl":
-		Permissions = GetPermissionMap(awsS3BucketACL, result.Attributes)
-	case "aws_s3_bucket_versioning":
-		Permissions = GetPermissionMap(awsS3BucketVersioning, result.Attributes)
-	case "aws_s3_bucket_server_side_encryption_configuration":
-		Permissions = GetPermissionMap(awsS3BucketServerSideEncryptionConfiguration, result.Attributes)
-	case "aws_s3_bucket_public_access_block":
-		Permissions = GetPermissionMap(awsS3BucketPublicAccessBlock, result.Attributes)
-	case "aws_instance":
-		Permissions = GetPermissionMap(awsInstance, result.Attributes)
-	case "aws_security_group":
-		Permissions = GetPermissionMap(awsSecurityGroup, result.Attributes)
-	case "aws_security_group_rule":
-		Permissions = GetPermissionMap(awsSecurityGroupRule, result.Attributes)
-	case "aws_lambda_function":
-		Permissions = GetPermissionMap(awsLambdaFunction, result.Attributes)
-	case "aws_vpc":
-		Permissions = GetPermissionMap(awsVpc, result.Attributes)
-	case "aws_subnet":
-		Permissions = GetPermissionMap(awsSubnet, result.Attributes)
-	case "aws_network_acl":
-		Permissions = GetPermissionMap(awsNetworkACL, result.Attributes)
-	case "aws_kms_key":
-		Permissions = GetPermissionMap(awsKmsKey, result.Attributes)
-	case "aws_iam_role":
-		Permissions = GetPermissionMap(awsIamRole, result.Attributes)
-	case "aws_iam_role_policy":
-		Permissions = GetPermissionMap(awsIamRolePolicy, result.Attributes)
-	case "aws_iam_role_policy_attachment":
-		Permissions = GetPermissionMap(awsIamRolePolicyAttachment, result.Attributes)
-	case "aws_iam_policy":
-		Permissions = GetPermissionMap(awsIamPolicy, result.Attributes)
-	case "aws_iam_instance_profile":
-		Permissions = GetPermissionMap(awsIamInstanceProfile, result.Attributes)
-	case "aws_iam_access_key":
-		Permissions = GetPermissionMap(awsIamAccessKey, result.Attributes)
-	case "aws_iam_group":
-		Permissions = GetPermissionMap(awsIamGroup, result.Attributes)
-	case "aws_iam_group_membership":
-		Permissions = GetPermissionMap(awsIamGroupMembership, result.Attributes)
-	case "aws_iam_group_policy":
-		Permissions = GetPermissionMap(awsIamGroupPolicy, result.Attributes)
-	case "aws_iam_group_policy_attachment":
-		Permissions = GetPermissionMap(awsIamGroupPolicyAttachment, result.Attributes)
-	case "aws_iam_policy_attachment":
-		Permissions = GetPermissionMap(awsIamPolicyAttachment, result.Attributes)
-	case "aws_iam_service_linked_role":
-		Permissions = GetPermissionMap(awsIamServiceLinkedRole, result.Attributes)
-	case "aws_iam_user":
-		Permissions = GetPermissionMap(awsIamUser, result.Attributes)
-	case "aws_iam_user_login_profile":
-		Permissions = GetPermissionMap(awsIamUserLoginProfile, result.Attributes)
-	case "aws_iam_user_policy":
-		Permissions = GetPermissionMap(awsIamUserPolicy, result.Attributes)
-	case "aws_iam_user_policy_attachment":
-		Permissions = GetPermissionMap(awsIamUserPolicyAttachment, result.Attributes)
-	case "aws_mq_broker":
-		Permissions = GetPermissionMap(awsMqBroker, result.Attributes)
-	case "aws_mq_configuration":
-		Permissions = GetPermissionMap(awsMqConfiguration, result.Attributes)
-	case "aws_cloudwatch_log_group":
-		Permissions = GetPermissionMap(awsCloudwatchLogGroup, result.Attributes)
-	case "aws_cloudwatch_event_rule":
-		Permissions = GetPermissionMap(awsCloudwatchEventRule, result.Attributes)
-	case "aws_cloudwatch_event_target":
-		Permissions = GetPermissionMap(awsCloudwatchEventTarget, result.Attributes)
-	case "aws_cloudwatch_log_metric_filter":
-		Permissions = GetPermissionMap(awsCloudwatchLogMetricFilter, result.Attributes)
-	case "aws_cloudwatch_log_resource_policy":
-		Permissions = GetPermissionMap(awsCloudwatchLogResourcePolicy, result.Attributes)
-	case "aws_cloudwatch_log_subscription_filter":
-		Permissions = GetPermissionMap(awsCloudwatchLogSubscriptionFilter, result.Attributes)
-	case "aws_cloudwatch_metric_alarm":
-		Permissions = GetPermissionMap(awsCloudwatchMetricAlarm, result.Attributes)
-	case "aws_route53_record":
-		Permissions = GetPermissionMap(awsRoute53Record, result.Attributes)
-	case "aws_sns_topic":
-		Permissions = GetPermissionMap(awsSnsTopic, result.Attributes)
-	case "aws_key_pair":
-		Permissions = GetPermissionMap(awsKeyPair, result.Attributes)
-	case "aws_db_instance":
-		Permissions = GetPermissionMap(awsDbInstance, result.Attributes)
-	case "aws_dynamodb_table":
-		Permissions = GetPermissionMap(awsDynamodbTable, result.Attributes)
-	case "aws_ssm_parameter":
-		Permissions = GetPermissionMap(awsSsmParameter, result.Attributes)
-	case "aws_route":
-		Permissions = GetPermissionMap(awsRoute, result.Attributes)
-	default:
-		log.Printf("%s not implemented", result.Name)
+
+	TFLookup := map[string]interface{}{
+		"aws_s3_bucket":            awsS3Bucket,
+		"aws_s3_bucket_acl":        awsS3BucketACL,
+		"aws_s3_bucket_versioning": awsS3BucketVersioning,
+		"aws_s3_bucket_server_side_encryption_configuration": awsS3BucketServerSideEncryptionConfiguration,
+		"aws_s3_bucket_public_access_block":                  awsS3BucketPublicAccessBlock,
+		"aws_instance":                                       awsInstance,
+		"aws_security_group":                                 awsSecurityGroup,
+		"aws_security_group_rule":                            awsSecurityGroupRule,
+		"aws_lambda_function":                                awsLambdaFunction,
+		"aws_vpc":                                            awsVpc,
+		"aws_subnet":                                         awsSubnet,
+		"aws_network_acl":                                    awsNetworkACL,
+		"aws_kms_key":                                        awsKmsKey,
+		"aws_iam_role":                                       awsIamRole,
+		"aws_iam_role_policy":                                awsIamRolePolicy,
+		"aws_iam_role_policy_attachment":                     awsIamRolePolicyAttachment,
+		"aws_iam_policy":                                     awsIamPolicy,
+		"aws_iam_instance_profile":                           awsIamInstanceProfile,
+		"aws_iam_access_key":                                 awsIamAccessKey,
+		"aws_iam_group":                                      awsIamGroup,
+		"aws_iam_group_membership":                           awsIamGroupMembership,
+		"aws_iam_group_policy":                               awsIamGroupPolicy,
+		"aws_iam_group_policy_attachment":                    awsIamGroupPolicyAttachment,
+		"aws_iam_policy_attachment":                          awsIamPolicyAttachment,
+		"aws_iam_service_linked_role":                        awsIamServiceLinkedRole,
+		"aws_iam_user":                                       awsIamUser,
+		"aws_iam_user_login_profile":                         awsIamUserLoginProfile,
+		"aws_iam_user_policy":                                awsIamUserPolicy,
+		"aws_iam_user_policy_attachment":                     awsIamUserPolicyAttachment,
+		"aws_mq_broker":                                      awsMqBroker,
+		"aws_mq_configuration":                               awsMqConfiguration,
+		"aws_cloudwatch_log_group":                           awsCloudwatchLogGroup,
+		"aws_cloudwatch_event_rule":                          awsCloudwatchEventRule,
+		"aws_cloudwatch_event_target":                        awsCloudwatchEventTarget,
+		"aws_cloudwatch_log_metric_filter":                   awsCloudwatchLogMetricFilter,
+		"aws_cloudwatch_log_resource_policy":                 awsCloudwatchLogResourcePolicy,
+		"aws_cloudwatch_log_subscription_filter":             awsCloudwatchLogSubscriptionFilter,
+		"aws_cloudwatch_metric_alarm":                        awsCloudwatchMetricAlarm,
+		"aws_route53_record":                                 awsRoute53Record,
+		"aws_sns_topic":                                      awsSnsTopic,
+		"aws_key_pair":                                       awsKeyPair,
+		"aws_db_instance":                                    awsDbInstance,
+		"aws_dynamodb_table":                                 awsDynamodbTable,
+		"aws_ssm_parameter":                                  awsSsmParameter,
+		"aws_route":                                          awsRoute,
 	}
+
+	var Permissions []string
+
+	temp := TFLookup[result.Name]
+	if temp != nil {
+		Permissions = GetPermissionMap(TFLookup[result.Name].([]byte), result.Attributes)
+	}
+
 	return Permissions
 }
 
--- src/aws_data_source.go
+++ src/aws_data_source.go
@@ -1,31 +1,31 @@
 package pike
 
-import "log"
-
 // GetAWSDataPermissions gets permissions required for datasources
 func GetAWSDataPermissions(result ResourceV2) []string {
-	var Permissions []string
-	switch result.Name {
-	case "aws_vpcs":
-		Permissions = GetPermissionMap(dataAwsVpcs, result.Attributes)
-	case "aws_subnet_ids", "aws_subnet", "aws_subnets":
-		Permissions = GetPermissionMap(dataAwsSubnetIds, result.Attributes)
-	case "aws_ami":
-		Permissions = GetPermissionMap(dataAwsAmi, result.Attributes)
-	case "aws_iam_policy":
-		Permissions = GetPermissionMap(dataAwsIamPolicy, result.Attributes)
-	case "aws_iam_role":
-		Permissions = GetPermissionMap(dataAwsIamRole, result.Attributes)
-	case "aws_s3_bucket":
-		Permissions = GetPermissionMap(dataAwsS3Bucket, result.Attributes)
-	case "aws_vpc":
-		Permissions = GetPermissionMap(dataAwsVpc, result.Attributes)
-	case "aws_availability_zones":
-		Permissions = GetPermissionMap(dataAwsAvailabilityZones, result.Attributes)
-	case "aws_caller_identity", "aws_iam_policy_document", "aws_region", "aws_canonical_user_id":
-		// do nothing
-	default:
-		log.Printf("%s.%s not implemented", result.TypeName, result.Name)
+
+	TFLookup := map[string]interface{}{
+		"aws_vpcs":                dataAwsVpcs,
+		"aws_subnet_ids":          dataAwsSubnetIds,
+		"aws_subnet":              dataAwsSubnetIds,
+		"aws_subnets":             dataAwsSubnetIds,
+		"aws_ami":                 dataAwsAmi,
+		"aws_iam_policy":          dataAwsIamPolicy,
+		"aws_iam_role":            dataAwsIamRole,
+		"aws_s3_bucket":           dataAwsS3Bucket,
+		"aws_vpc":                 dataAwsVpc,
+		"aws_availability_zones":  dataAwsAvailabilityZones,
+		"aws_caller_identity":     nil,
+		"aws_iam_policy_document": nil,
+		"aws_region":              nil,
+		"aws_canonical_user_id":   nil,
 	}
+
+	var Permissions []string
+
+	temp := TFLookup[result.Name]
+	if temp != nil {
+		Permissions = GetPermissionMap(TFLookup[result.Name].([]byte), result.Attributes)
+	}
+
 	return Permissions
 }
