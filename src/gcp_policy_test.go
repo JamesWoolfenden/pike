9e913c96856f6695604bd6af0961aa48ea6282ee
James Woolfenden <james.woolfenden@gmail.com>
2022-09-12 15:32:01 +0100

    win test

--- src/gcp_policy_test.go
+++ src/gcp_policy_test.go
@@ -17,7 +17,7 @@ func TestGCPPolicy(t *testing.T) {
 		wantErr bool
 	}{
 		{"basic", args{[]string{"bigquery.datasets.create", "bigquery.jobs.create"}},
-			"resource \"google_project_iam_custom_role\" \"terraformXVlBzgba\" {\n  project     = \"pike\"\n  role_id     = \"terraform_pike\"\n  title       = \"terraformXVlBzgba\"\n  description = \"A user with least privileges\"\n  permissions = [\n    \"bigquery.datasets.create\",\n    \"bigquery.jobs.create\"\n]\n}",
+			"resource \"google_project_iam_custom_role\" \"terraformXVlBzgba\" {\n  project     = \"pike\"\n  role_id     = \"terraform_pike\"\n  title       = \"terraformXVlBzgba\"\n  description = \"A user with least privileges\"\n  permissions = [\n    \"bigquery.datasets.create\",\n    \"bigquery.jobs.create\"\n  ]\n}\n",
 			false},
 	}
 	for _, tt := range tests {
