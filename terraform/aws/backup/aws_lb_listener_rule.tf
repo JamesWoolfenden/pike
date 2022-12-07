f916e8bd31cceb08fdd8d573fd5691ed8279abfa
James Woolfenden <james.woolfenden@gmail.com>
2022-12-07 18:58:58 +0000

    add test resource for aws_lb_listener

---
+++ terraform/aws/backup/aws_lb_listener_rule.tf
@@ -0,0 +1,21 @@
+resource "aws_lb_listener_rule" "pike" {
+  listener_arn = aws_lb_listener.front_end.arn
+  priority     = 100
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.static.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/static/*"]
+    }
+  }
+
+  condition {
+    host_header {
+      values = ["example.com"]
+    }
+  }
+}
