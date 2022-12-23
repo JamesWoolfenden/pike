2391b322d62ee8c4d796cbfd539289b37fff2101
James Woolfenden <james.woolfenden@gmail.com>
2022-12-23 12:53:56 +0000

    better error handling

--- src/gitHub.go
+++ src/gitHub.go
@@ -1,7 +1,9 @@
 package pike
 
 import (
+	"context"
 	"errors"
+	"net/http"
 	"strconv"
 
 	"github.com/rs/zerolog/log"
@@ -12,12 +14,26 @@ import (
 // InvokeGithubDispatchEvent uses your gitHub api key (if sufficiently enabled) to invoke a gitHub action workflow
 func InvokeGithubDispatchEvent(repository string, workflowFileName string, branch string) error {
 	owner, repo, err := splitHub(repository)
-
 	if err != nil {
+		log.Print(err)
 		return err
 	}
 
+	url := "https://api.github.com/repos/" + repository + "/actions/workflows/" + workflowFileName
+
+	err2 := VerifyURL(url)
+	if err2 != nil {
+		log.Print(err2)
+		return err2
+	}
+
 	ctx, client := getGithubClient()
+
+	err3 := VerifyBranch(client, owner, repo, branch)
+	if err3 != nil {
+		log.Print(err3)
+		return err3
+	}
 
 	event := github.CreateWorkflowDispatchEventRequest{
 		Ref: branch,
@@ -31,7 +47,7 @@ func InvokeGithubDispatchEvent(repository string, workflowFileName string, branc
 		event)
 
 	if err != nil {
-		log.Print(response)
+		log.Printf("invoke failed %s", response.Response.Status)
 		return err
 	}
 
@@ -43,8 +59,40 @@ func InvokeGithubDispatchEvent(repository string, workflowFileName string, branc
 		if left == 0 {
 			return errors.New("you are being gitHub rate limited")
 		}
-		log.Printf("Github rate limit: %s", remains[0])
+		log.Printf("Invoked: Github rate limit remaining: %s", remains[0])
 	}
 
+	return nil
+}
+
+// VerifyBranch checks that a branch exists in a repo
+func VerifyBranch(client *github.Client, owner string, repo string, branch string) error {
+	ctx := context.Background()
+	branches, _, _ := client.Repositories.ListBranches(ctx, owner, repo, nil)
+	found := false
+
+	for _, stem := range branches {
+		temp := *stem
+		if *temp.Name == branch {
+			found = true
+		}
+	}
+	if found {
+		return nil
+	}
+	return errors.New("branch " + branch + " not found for " + repo)
+}
+
+// VerifyURL tests a url
+func VerifyURL(url string) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Printf("failed to reach %s for %s", url, resp.Status)
+		return err
+	}
+	if resp.StatusCode != http.StatusOK {
+		log.Printf("non ok status code %s for %s", resp.Status, url)
+		return errors.New(resp.Status)
+	}
 	return nil
 }
