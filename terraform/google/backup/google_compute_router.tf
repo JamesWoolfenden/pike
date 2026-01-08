1f59b0807aeb52c1f410753246355ad4e029abb0
James Woolfenden <james.woolfenden@gmail.com>
2026-01-08 08:27:58 +0000

    add test data for new resources

---
+++ terraform/google/backup/google_compute_router.tf
@@ -0,0 +1,9 @@
+resource "google_compute_router" "router" {
+  name    = "my-router"
+  region  = google_compute_subnetwork.subnet.region
+  network = google_compute_network.net.id
+
+  bgp {
+    asn = 64514
+  }
+}
---
+++ terraform/google/backup/google_compute_router_nat.tf
@@ -0,0 +1,12 @@
+resource "google_compute_router_nat" "nat" {
+  name                               = "my-router-nat"
+  router                             = google_compute_router.router.name
+  region                             = google_compute_router.router.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+
+  log_config {
+    enable = true
+    filter = "ERRORS_ONLY"
+  }
+}
